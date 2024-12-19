97cdf39f55b7fc488ccc095e77beec7c4c032a5a
kevinoriley <109296879+kevinoriley@users.noreply.github.com>
2024-12-19 11:33:01 -0500

    Update simple_ipaddress_iterator.py

--- chapter03/simple_ipaddress_iterator.py
+++ chapter03/simple_ipaddress_iterator.py
@@ -1,1 +1,7 @@
+import ipaddress
+#OR: from ipaddress import ip_address, ip_network
 
+for ip in Ipv4Network("192.168.100.1/24"):
+  s = socket.socket()
+  s.connect((ip, 25))
+  # send smtp packets
